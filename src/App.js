89b80b55c6b903c14d3d33aedb036b5995c3c68c
Vanessa Dixon <vanessazdixon@gmail.com>
2021-01-03 02:07:02 -0500

    completed mvp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Character from './components/Character';
-// import Card from './components/Card';
+
 
 
 const App = () => {
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
 
 const Card = ({id, name, status,  gender, image, species}) => {
+
+    const WrapperDiv = styled.div`
+       display: inline-block;
+        width: 20%;
+        margin: 0 4%;
+        padding: 0 1%;
+    `
+
     return (
-        <div id={id}>
+        <WrapperDiv id={id}>
             <h1>{name}</h1>
             <img src={image} alt='character' />
             <h3>Status: {status}</h3>
             <h3>Gender: {gender}</h3>
             <h3>Species: {species}</h3>
-        </div>
+        </WrapperDiv>
     )
 }
 
